e9b361c48dd2c7af8be4da9c941e629c4ef5f8ea
Vishwas Dubey <VishwasDubey150@gmail.com>
2026-04-02 14:47:40 +0100

    debugged 3

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["http://98.88.37.221:5173", "http://98.88.37.221"]
+CORS_ALLOWED_ORIGINS = ["http://98.88.37.221", # Frontend IP
+    "http://localhost:3000"]
 
 ROOT_URLCONF = "core.urls"
 
